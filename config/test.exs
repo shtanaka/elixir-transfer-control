93bc8f8adc87da914f984c55f72c928f7d8576bd
Gustavo Santiago <egustavo4@gmail.com>
2019-12-27 17:40:56 -0300

    Add environment based test config

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,8 @@ config :tfcon, Tfcon.Repo,
   username: "postgres",
   password: "postgres",
   database: "tfcon_test",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST"),
+  port: System.get_env("DB_PORT"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
