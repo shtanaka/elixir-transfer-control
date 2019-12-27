60cb66da096d77f2e02187aa86510acfe2bd18c0
Gustavo Santiago <egustavo4@gmail.com>
2019-12-27 17:49:24 -0300

    Configure 404 status

--- apps/tfcon_web/lib/tfcon_web/controllers/user_controller.ex
+++ apps/tfcon_web/lib/tfcon_web/controllers/user_controller.ex
@@ -8,9 +8,12 @@ defmodule TfconWeb.UserController do
   end
 
   def show(conn, %{"account_number" => account_number}) do
-    {account_number, ""} = Integer.parse(account_number)
-    account = Accounts.get_user_by_account_number!(account_number)
+    {account_number, _} = Integer.parse(account_number)
+    user = Accounts.get_user_by_account_number!(account_number)
 
-    render(conn, "show.json", account)
+    render_show(conn, user)
   end
+
+  defp render_show(conn, nil), do: conn |> put_status(404) |> render("404.json", %{user: nil})
+  defp render_show(conn, user), do: conn |> render("show.json", %{user: user})
 end
--- apps/tfcon_web/lib/tfcon_web/views/user_view.ex
+++ apps/tfcon_web/lib/tfcon_web/views/user_view.ex
@@ -3,6 +3,7 @@ defmodule TfconWeb.UserView do
   @minimal_attrs ~W(name account_number)a
   # @expanded_attrs ~W(name account_number balance)a
 
-  def render("show.json", user), do: user |> Map.take(@minimal_attrs)
+  def render("show.json", %{user: user}), do: user |> Map.take(@minimal_attrs)
   def render("index.json", %{users: users}), do: Enum.map(users, fn user -> Map.take(user, @minimal_attrs) end)
+  def render("404.json", _), do: %{"error" => "Not found"}
 end
