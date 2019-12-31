4b03cdbf531e7e72aaff6c164ed3d888b22256ef
Gustavo Santiago <egustavo4@gmail.com>
2019-12-31 18:11:38 -0300

    Add error handling to account not found bank controller

--- apps/tfcon_web/lib/tfcon_web/views/bank_view.ex
+++ apps/tfcon_web/lib/tfcon_web/views/bank_view.ex
@@ -6,6 +6,22 @@ defmodule TfconWeb.BankView do
   @minimal_attrs ~W(name account_number)a
   @expanded_attrs ~W(name account_number balance)a
 
+  def render("404.json", _) do
+    JsonHandler.error_json(%{"errors" => ["Entity not found."]})
+  end
+  def render("no_balance.json", %{changeset: changeset}) do
+    errors =
+      Changeset.traverse_errors(changeset, fn
+        {msg, opts} ->
+          Enum.reduce(opts, msg, fn {key, value}, acc ->
+            String.replace(acc, "%{#{key}}", to_string(value))
+          end)
+        msg ->
+          msg
+      end)
+
+    JsonHandler.error_json(%{"errors" => errors})
+  end
   def render("my_account.json", %{user: user}) do
     user_data = Map.take(user, @expanded_attrs)
     JsonHandler.success_json(%{user: user_data})
@@ -28,18 +44,5 @@ defmodule TfconWeb.BankView do
       user: user_data,
       message: "value successfully debited from #{user.account_number}."
     })
-  end
-  def render("no_balance.json", %{changeset: changeset}) do
-    errors =
-      Changeset.traverse_errors(changeset, fn
-        {msg, opts} ->
-          Enum.reduce(opts, msg, fn {key, value}, acc ->
-            String.replace(acc, "%{#{key}}", to_string(value))
-          end)
-        msg ->
-          msg
-      end)
-
-    JsonHandler.error_json(%{"errors" => errors})
   end
 end
